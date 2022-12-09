1d5eff8d56859a812c3d4845fbe77f7d571d5410
YashParas <100356832+YashParas@users.noreply.github.com>
2022-12-09 10:40:21 +0530

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n);
+{
+	if(n<0)
+	return -1;
+	if(n==0)
+	return 1;
+	else
+	{
+		return(n*factorial(n-1));
+	}
+	
+}
+int main()
+{
+	int factorial(int);
+	int fact, num;
+	cout<<"Enter any number";
+	cin>>num;
+	n=factorial(n);
+	cout<<"Factorial of"<<" "<<n<<" is : "<<fact<<endl;
+}
